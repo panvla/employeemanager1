f67cb7c62233ac0313b956c67c2828d24138d179
panvla <pandvlado@gmail.com>
2023-01-02 16:57:07 +0100

    added backend stuff

---
+++ employeemanager1/src/main/java/com/vladimirpandurov/employeemanager1/Employeemanager1Application.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.employeemanager1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeemanager1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeemanager1Application.class, args);
+	}
+
+}
