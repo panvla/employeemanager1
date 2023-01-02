139cec984e4dbec65ef5164df7c47638cca9ee96
panvla <pandvlado@gmail.com>
2023-01-02 14:45:07 +0100

    added initial project

---
+++ employeemanager1/src/main/java/com/vladimirpandurov/employeemanager1/model/Employee.java
@@ -0,0 +1,27 @@
+package com.vladimirpandurov.employeemanager1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+}
