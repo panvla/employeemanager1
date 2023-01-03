7b9d4c57a7899c8cc6a30a30c283a2ed82ff1fa5
panvla <pandvlado@gmail.com>
2023-01-03 22:38:21 +0100

    finish

---
+++ employeemanager1/src/test/java/com/vladimirpandurov/employeemanager1/Employeemanager1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.employeemanager1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Employeemanager1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
